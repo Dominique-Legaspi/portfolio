1e101b54f0a8e688099eac45a218a29aec0113ec
Dominique-Legaspi <domi.legaspi1006@gmail.com>
2024-09-24 18:41:06 -0700

    mobile nav

--- components/BlindsTransition.jsx
+++ components/BlindsTransition.jsx
@@ -17,6 +17,13 @@ const BlindsTransition = () => {
                         <Blinds/>
                     </div>
                 </div>
+{/* 
+                <motion.div 
+                className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" 
+                initial={{opacity: 1}}
+                animate={{opacity: 0, transition: {delay: 0.2, duration: 0.4, ease: 'easeInOut'}}}
+                /> */}
+
             </AnimatePresence>
         </>
     )
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,10 @@ import { usePathname } from "next/navigation";
 
 const links = [
     {
+        name: "home",
+        path: "/"
+    },
+    {
         name: 'about',
         path: "/about",
     },
